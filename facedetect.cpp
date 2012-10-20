ed862a9d74075b0ad03634f4e8d9c40b757b89ff
awright <akw0088@unt.edu>
2012-10-20 15:39:59 -0500

    Fix memory leak

--- facedetect.cpp
+++ facedetect.cpp
@@ -1,6 +1,12 @@
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 
+/*
+Win32 compile
+cl facedetect.cpp /EHsc /I c:\opencv\build\include\ /link c:\opencv\build\x86\vc10\lib\opencv_core242.lib c:\opencv\build\x86\vc10\lib\opencv_highgui242.lib c:\opencv\build\x86\vc10\lib\opencv_objdetect242.lib
+
+requires Dll's from: C:\opencv\build\x86\vc10\bin and C:\opencv\build\common\tbb\ia32\vc10
+*/
 
 // C++
 #include <iostream>
@@ -27,38 +33,38 @@ static CvHaarClassifierCascade* cascade = 0;
 // Function to detect and draw any faces that is present in an image
 void detect_and_draw( IplImage* img )
 {
-    int scale = 1;
-    IplImage* temp = cvCreateImage( cvSize( img->width / scale, img->height / scale ), 8, 3 );
-    CvPoint pt1, pt2;
+	int scale = 1;
+	IplImage* temp = cvCreateImage( cvSize( img->width / scale, img->height / scale ), 8, 3 );
+	CvPoint pt1, pt2;
 
-    cvClearMemStorage( storage );
+	cvClearMemStorage( storage );
 
-    if( cascade )
+	if( cascade )
 	{
-        // There can be more than one face in an image. So create a growable sequence of faces.
-        // Detect the objects and store them in the sequence
-        CvSeq* faces = cvHaarDetectObjects( img, cascade, storage,
+		// There can be more than one face in an image. So create a growable sequence of faces.
+		// Detect the objects and store them in the sequence
+		CvSeq* faces = cvHaarDetectObjects( img, cascade, storage,
                                             1.1, 2, CV_HAAR_DO_CANNY_PRUNING,
                                             cvSize( 40, 40 ) );
 
-        // Loop the number of faces found.
-        for( int i = 0; i < ( faces ? faces->total : 0 ); i++ )
+		// Loop the number of faces found.
+		for( int i = 0; i < ( faces ? faces->total : 0 ); i++ )
 		{
-            // Create a new rectangle for drawing the face
-            CvRect* r = ( CvRect* )cvGetSeqElem( faces, i );
+			// Create a new rectangle for drawing the face
+			CvRect* r = ( CvRect* )cvGetSeqElem( faces, i );
 
-            // Find the dimensions of the face,and scale it if necessary
-            pt1.x = r->x * scale;
-            pt2.x = ( r->x + r->width ) * scale;
-            pt1.y = r->y * scale;
-            pt2.y = ( r->y + r->height ) * scale;
+			// Find the dimensions of the face,and scale it if necessary
+			pt1.x = r->x * scale;
+			pt2.x = ( r->x + r->width ) * scale;
+			pt1.y = r->y * scale;
+			pt2.y = ( r->y + r->height ) * scale;
 
-            // Draw the rectangle in the input image
-            cvRectangle( img, pt1, pt2, CV_RGB( 255, 0, 0 ), 3, 8, 0 );
-        }
-    }
+			// Draw the rectangle in the input image
+			cvRectangle( img, pt1, pt2, CV_RGB( 255, 0, 0 ), 3, 8, 0 );
+		}
+	}
 
-    cvReleaseImage( &temp );
+	cvReleaseImage( &temp );
 }
 
 // Main function, defines the entry point for the program.
@@ -76,31 +82,33 @@ int main( int argc, char** argv )
 
 	CvCapture *capture;
 
+	// Load the HaarClassifierCascade
+	cascade = ( CvHaarClassifierCascade* )cvLoad("haarcascade_frontalface_alt.xml", 0, 0, 0 );
+
+	if( !cascade )
+	{
+		cerr << "ERROR: Could not load classifier cascade" << endl;
+		return -1;
+	}
+
+
 	while ( key != 27 )
 	{
 		capture = cvCaptureFromCAM(0);
 
-        // Load the HaarClassifierCascade
-        cascade = ( CvHaarClassifierCascade* )cvLoad("haarcascade_frontalface_alt.xml", 0, 0, 0 );
+		storage = cvCreateMemStorage( 0 );
+		image = cvQueryFrame( capture );
 
-        if( !cascade )
-		{
-            cerr << "ERROR: Could not load classifier cascade" << endl;
-            return -1;
-        }
-
-        storage = cvCreateMemStorage( 0 );
-        image = cvQueryFrame( capture );
-
-        if(!image)
+		if(!image)
 		{
 			break;
-        }
+		}
 		detect_and_draw( image );
 		cvNamedWindow( "video", 1 );
 		cvShowImage( "video", image );
+		cvClearMemStorage( storage );
 		key = cvWaitKey( 20 );
-   }
+	}
 
-    return 0;
+	return 0;
 }
